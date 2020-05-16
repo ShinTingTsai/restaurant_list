d6f754f0d26d08ee5e9c47a138713b812e2ee778
STTSAI <shinting.tsai@gmail.com>
2020-05-16 17:18:04 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
-const exphbs = require('express-handlebars')
+
+mongoose.connect("mongodb://localhost/restaurant-list", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 const restaurantList = require('./restaurant.json')
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
- res.render("index", { restaurants: restaurantList.results } )
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter( restaurant => {
+  const restaurants = restaurantList.results.filter(restaurant => {
     return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
   })
   if (Object.keys(restaurants).length === 0) {
-    res.render("notFound", { keyword: keyword });
+    res.render('notFound', { keyword: keyword })
   } else {
-    res.render("index", { restaurants: restaurants, keyword: keyword });
+    res.render('index', { restaurants: restaurants, keyword: keyword })
   }
 })
 
-app.get("/restaurants/:id", (req, res) => {
+app.get('/restaurants/:id', (req, res) => {
   const restaurant = restaurantList.results.find(
     (restaurant) => restaurant.id.toString() === req.params.id
-  );
-  res.render("show", { restaurant: restaurant });
-});
+  )
+  res.render('show', { restaurant: restaurant })
+})
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
