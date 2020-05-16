86394b6e7bd2b96e4e7cf3c000cb5b71b6b26ebb
STTSAI <shinting.tsai@gmail.com>
2020-05-16 18:13:32 +0800

    feat: change data source from .json to db

--- app.js
+++ app.js
@@ -5,49 +5,54 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
+const Restaurant = require("./models/restaurant");
+
+// 資料庫連線
 mongoose.connect("mongodb://localhost/restaurant-list", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-// 取得資料庫連線狀態
 const db = mongoose.connection
-// 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-const restaurantList = require('./restaurant.json')
+// const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
-})
+//Setup Router
+app.get("/", (req, res) => {
+  Restaurant.find()
+  .lean()
+  .then(restaurants => res.render('index', { restaurants }))
+  .catch(error => console.error(error))
+});
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-  })
-  if (Object.keys(restaurants).length === 0) {
-    res.render('notFound', { keyword: keyword })
-  } else {
-    res.render('index', { restaurants: restaurants, keyword: keyword })
-  }
-})
 
-app.get('/restaurants/:id', (req, res) => {
-  const restaurant = restaurantList.results.find(
-    (restaurant) => restaurant.id.toString() === req.params.id
-  )
-  res.render('show', { restaurant: restaurant })
-})
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword
+//   const restaurants = restaurantList.results.filter(restaurant => {
+//     return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+//   })
+//   if (Object.keys(restaurants).length === 0) {
+//     res.render('notFound', { keyword: keyword })
+//   } else {
+//     res.render('index', { restaurants: restaurants, keyword: keyword })
+//   }
+// })
+
+// app.get('/restaurants/:id', (req, res) => {
+//   const restaurant = restaurantList.results.find(
+//     (restaurant) => restaurant.id.toString() === req.params.id
+//   )
+//   res.render('show', { restaurant: restaurant })
+// })
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
