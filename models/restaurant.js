4ff633b97dfca5f9c7179307886439dcaef5a83c
STTSAI <shinting.tsai@gmail.com>
2020-05-16 17:39:59 +0800

    feat: add restaurant schema

---
+++ models/restaurant.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+  id: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    required: false
+  },
+  name_en: {
+    type: String,
+    required: false
+  },
+  category: {
+    type: String,
+    required: false
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  location: {
+    type: String,
+    required: false
+  },
+  phone: {
+    type: String,
+    required: false
+  },
+  google_map: {
+    type: String,
+    required: false
+  },
+  rating: {
+    type: Number,
+    required: false
+  },
+  description: {
+    type: String,
+    required: false
+  }
+})
+module.exports = mongoose.model('Restaurant', restaurantSchema)
