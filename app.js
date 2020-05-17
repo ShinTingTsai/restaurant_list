6b7d7b699e7bdb3dcad1f44812820d8a46e8f69d
STTSAI <shinting.tsai@gmail.com>
2020-05-17 19:18:38 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 // 引用路由器
 const routes = require('./routes')
-
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
-
-const Restaurant = require('./models/restaurant')
-
-// 資料庫連線
-mongoose.connect('mongodb://localhost/restaurant-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+// const Restaurant = require('./models/restaurant')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride("_method"));
-
-// 將 request 導入路由器
+app.use(methodOverride('_method'))
 app.use(routes)
-
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const express = require('express')
 const router = express.Router()
 
 // 準備引入路由模組
-const home = require("./modules/home");
-const restaurants = require("./modules/restaurants");
-// 將網址結構符合 / 字串的 request 導向 home 模組 
+const home = require('./modules/home')
+const restaurants = require('./modules/restaurants')
+// 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
 
-// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
+// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組
 router.use('/restaurants', restaurants)
 
 // 匯出路由器
