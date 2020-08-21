68db875ffe294a5ffb13fe05d5afc664b732cb4d
STTSAI <shinting.tsai@gmail.com>
2020-08-22 01:00:35 +0800

    feat: add users modules and upload snapshot

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const router = express.Router()
 // 準備引入路由模組
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
+
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
-
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
