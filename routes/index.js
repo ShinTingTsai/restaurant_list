e4cd6c112427cfea83961ae9a63252d66627a7f0
STTSAI <shinting.tsai@gmail.com>
2020-08-23 03:31:08 +0800

    feat: add isAuthenticated

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
-const session = require('express-session')
 const router = express.Router()
-
 
 // 準備引入路由模組
 const home = require('./modules/home')
@@ -10,14 +8,7 @@ const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
 const { authenticator } = require('../middleware/auth')
 
-router.use(session({
-  secret: 'ThisIsMySecret',
-  resave: false,
-  saveUninitialized: true
-}))
-
-
-// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組
+// 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
 router.use('/restaurants', authenticator, restaurants)
 router.use('/users', users)
 // 將網址結構符合 / 字串的 request 導向 home 模組
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
-const passport = require("passport");
+const passport = require('passport')
 
 router.get('/login', (req, res) => {
   res.render('login')
