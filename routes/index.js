c21095e00bf348ee6f08aa438acede47f0c267d1
STTSAI <shinting.tsai@gmail.com>
2020-08-22 13:18:54 +0800

    feat: add express-session

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
+const session = require('express-session')
 const router = express.Router()
+
 
 // 準備引入路由模組
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+
+router.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
