1be69b4e0eb7bfa11a651b4a706ebdd194696a1c
STTSAI <shinting.tsai@gmail.com>
2020-08-23 22:56:58 +0800

    modify seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,11 +1,69 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const User = require('../user')
 const Restaurant = require('../restaurant')
 const restaurantList = require('./restaurant.json')
 const db = require('../../config/mongoose')
 
-db.once('open', () => {
-  console.log('mongodb connected!')
-  for (i = 0; i < restaurantList.results.length; i++) {
-    Restaurant.create(restaurantList.results[i])
+const seedUsers = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678'
   }
-  console.log('Restaurant create done')
+]
+
+db.once('open', () => {
+  function createData (user, restaurantId) {
+    return new Promise((resolve, reject) => {
+      bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(user.password, salt))
+        .then(hash => User.create({
+          name: user.name,
+          email: user.email,
+          password: hash
+        }))
+        .then(user => {
+          const userId = user._id
+          return Promise.all(Array.from(
+            { length: 3 },
+            (_, i) => Restaurant.create({
+              name: restaurantList.results[i + restaurantId].name,
+              name_en: restaurantList.results[i + restaurantId].name_en,
+              category: restaurantList.results[i + restaurantId].category,
+              image: restaurantList.results[i + restaurantId].image,
+              location: restaurantList.results[i + restaurantId].location,
+              phone: restaurantList.results[i + restaurantId].phone,
+              google_map: restaurantList.results[i + restaurantId].google_map,
+              rating: restaurantList.results[i + restaurantId].rating,
+              description: restaurantList.results[i + restaurantId].description,
+              userId
+            })
+          )).then(() => resolve())
+        })
+    }).catch(err => console.log(err))
+  }
+
+  async function seeder (seedUsers) {
+    try {
+      await createData(seedUsers[0], 0)
+      await createData(seedUsers[1], 3)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  seeder(seedUsers)
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
